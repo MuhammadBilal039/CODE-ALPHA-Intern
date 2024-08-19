a0c797507b7a0656aa54bfeba7a2cbfa7dffe1e8
Muhammad Bilal <150532599+MuhammadBilal039@users.noreply.github.com>
2024-08-19 22:02:35 +0500

    Add files via upload
    Music Player of Songs

---
+++ Task Fourth Music Player/main.js
@@ -0,0 +1,37 @@
+let progress = document.getElementById("progress");
+
+let song = document.getElementById("song");
+
+let controlIcon = document.getElementById("controlIcon");
+
+song.onloadedmetadata = function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+};
+
+function playPause() {
+  if (controlIcon.classList.contains("fa-pause")) {
+    song.pause();
+    controlIcon.classList.remove("fa-pausse");
+    controlIcon.classList.add("fa-play");
+  } 
+  else {
+    song.play(); 
+    controlIcon.classList.add("fa-pause");
+    controlIcon.classList.remove("fa-play");
+  }
+}
+
+if (song.play()) {
+  setInterval(() => {
+    progress.value = song.currentTime;
+  }, 500);
+}
+
+progress.onchange = function () {
+    song.play();
+    song.currentTime = progress.value;
+    controlIcon.classList.add("fa-pause");
+    controlIcon.classList.remove("fa-play");
+}
+
